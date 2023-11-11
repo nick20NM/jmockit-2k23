c6f626db439271d5ce8c0b3a35bfab9075e5c466
NM <masihnishit@gmail.com>
2023-11-11 21:23:51 +0530

    video 2

--- src/main/java/com/alpha/www/JMockit/pack1/Calculator.java
+++ src/main/java/com/alpha/www/JMockit/pack1/Calculator.java
@@ -1,6 +1,25 @@
 package com.alpha.www.JMockit.pack1;
 
 public class Calculator {
+	
+	public String publicString;
+	private String privateString;
+	public final String finalString="finalString";
+	String string="string";
+	int a;
+	float b;
+
+	/* will throw -> java.lang.IllegalArgumentException: No constructor in tested 
+	class that can be satisfied by available tested/injectable values */
+//	public Calculator(int a, float b) { 
+//		super();
+//		this.a = a;
+//		this.b = b;
+//	}
+	
+	public Calculator() {
+		
+	}
 
 	public double performMathOperation(String operation, double a, double b) {
 		double result = 0;
--- src/test/java/com/alpha/www/JMockit/pack1/CalculatorTest.java
+++ src/test/java/com/alpha/www/JMockit/pack1/CalculatorTest.java
@@ -11,42 +11,41 @@ import mockit.Tested;
 
 public class CalculatorTest {
 
-	@Tested
-//	@Mocked
+	@Tested // @Tested -> Calculator calculator = new Calculator();
 	Calculator calculator;
-	
+
 	@Test
 	@DisplayName("testAdditionOperation")
 	void testAdditionOperation() {
 		double result = calculator.performMathOperation("add", 2, 2);
 		assertTrue(result > 0);
 	}
-	
+
 	@Test
 	@DisplayName("testSubtractionOperation")
 	void testSubtractionOperation() {
 		double result = calculator.performMathOperation("sub", 5, 2);
 		assertTrue(result > 0);
 	}
-	
+
 	@Test
 	@DisplayName("testMultiplicationOperation")
 	void testMultiplicationOperation() {
 		double result = calculator.performMathOperation("mul", 2, 2);
 		assertTrue(result > 0);
 	}
-	
+
 	@Test
 	@DisplayName("testDivisionOperation")
 	void testDivisionOperation() {
 		double result = calculator.performMathOperation("div", 10, 2);
 		assertTrue(result > 0);
 	}
-	
+
 	@Test
 	@DisplayName("testInvalidOperation")
 	void testInvalidOperation() {
 		RuntimeException exception = assertThrows(RuntimeException.class,
-                () -> calculator.performMathOperation("invalidOperation", 10, 2));
+				() -> calculator.performMathOperation("invalidOperation", 10, 2));
 	}
 }
